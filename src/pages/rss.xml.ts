dc98a171303d26448ad8631439fdd256adb582c4
lawvs <18554747+lawvs@users.noreply.github.com>
2025-11-14 20:07:21 +0800

    refactor: sync to upstream

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,43 +1,42 @@
-import { siteConfig } from '@/config'
-import rss from '@astrojs/rss'
-import { getSortedPosts } from '@utils/content-utils'
-import type { APIContext } from 'astro'
-import MarkdownIt from 'markdown-it'
-import sanitizeHtml from 'sanitize-html'
+import rss from "@astrojs/rss";
+import { getSortedPosts } from "@utils/content-utils";
+import { url } from "@utils/url-utils";
+import type { APIContext } from "astro";
+import MarkdownIt from "markdown-it";
+import sanitizeHtml from "sanitize-html";
+import { siteConfig } from "@/config";
 
-const parser = new MarkdownIt()
+const parser = new MarkdownIt();
+
+function stripInvalidXmlChars(str: string): string {
+	return str.replace(
+		// biome-ignore lint/suspicious/noControlCharactersInRegex: https://www.w3.org/TR/xml/#charsets
+		/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F-\x9F\uFDD0-\uFDEF\uFFFE\uFFFF]/g,
+		"",
+	);
+}
 
 export async function GET(context: APIContext) {
-  const blog = await getSortedPosts()
-  const { site } = context
+	const blog = await getSortedPosts();
 
-  if (!site) {
-    throw new Error('Missing site metadata')
-  }
-
-  return rss({
-    title: siteConfig.title,
-    description: siteConfig.subtitle || 'No description',
-    site,
-    items: blog
-      .map((post) => {
-        return {
-          title: post.data.title,
-          pubDate: post.data.published,
-          description: post.data.description || '',
-          link: `/posts/${post.slug}/`,
-          content: sanitizeHtml(parser.render(post.body), {
-            // https://stackoverflow.com/questions/12229572/php-generated-xml-shows-invalid-char-value-27-message
-            textFilter: (text, tagName) =>
-              text.replace(
-                /[^\x09\x0A\x0D\x20-\xFF\x85\xA0-\uD7FF\uE000-\uFDCF\uFDE0-\uFFFD]/gm,
-                '',
-              ),
-            allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
-          }),
-        }
-      })
-      .slice(0, 20),
-    customData: `<language>${siteConfig.lang}</language>`,
-  })
+	return rss({
+		title: siteConfig.title,
+		description: siteConfig.subtitle || "No description",
+		site: context.site ?? "https://fuwari.vercel.app",
+		items: blog.map((post) => {
+			const content =
+				typeof post.body === "string" ? post.body : String(post.body || "");
+			const cleanedContent = stripInvalidXmlChars(content);
+			return {
+				title: post.data.title,
+				pubDate: post.data.published,
+				description: post.data.description || "",
+				link: url(`/posts/${post.slug}/`),
+				content: sanitizeHtml(parser.render(cleanedContent), {
+					allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+				}),
+			};
+		}),
+		customData: `<language>${siteConfig.lang}</language>`,
+	});
 }
