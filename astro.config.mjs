ec0e7d97b0976353a57c67715511e30daf8f6dd9
lawvs <18554747+lawvs@users.noreply.github.com>
2025-11-27 15:28:41 +0800

    fix: update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://www.waterwater.moe/",
   base: "/",
   trailingSlash: "always",
   integrations: [
