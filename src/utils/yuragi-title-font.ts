a79c2a25db6d303161e9f995f62b0fafb24992b6
lawvs <18554747+lawvs@users.noreply.github.com>
2026-07-27 18:23:45 +0800

    chore: load yuragi title font from jsdelivr

--- src/utils/yuragi-title-font.ts
+++ src/utils/yuragi-title-font.ts
@@ -1,7 +1,7 @@
 import { createYuragiFont, type YuragiFont } from "@yuragi-labs/core/wasm";
 
 const YURAGI_TITLE_FONT_URL =
-	"https://raw.githubusercontent.com/adobe-fonts/source-han-sans/a4f7cf94edfb9d7ffbdfc4841de276358bd7e0f2/SubsetOTF/CN/SourceHanSansCN-Bold.otf";
+	"https://cdn.jsdelivr.net/gh/adobe-fonts/source-han-sans@a4f7cf94edfb9d7ffbdfc4841de276358bd7e0f2/SubsetOTF/CN/SourceHanSansCN-Bold.otf";
 
 let fontPromise: Promise<YuragiFont> | undefined;
 let cachedFont: YuragiFont | undefined;
